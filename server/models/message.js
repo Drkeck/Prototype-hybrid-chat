9eea8e8217a545e3e6c36ddd31e6d7722cc29a9a
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-02-01 14:55:06 -0800

    i am realizing now that i haven't built a schema for messages before, i need to look into some info that might be important to store.

---
+++ server/models/message.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose
+
+const messageSchema = new Schema({
+    messageBody: {
+        type: String,
+        required: true
+    },
+    timeSent: {
+        type: Date,
+        default: Date.now
+    },
+    
+})
