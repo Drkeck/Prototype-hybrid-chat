133f15999e89b4c95980b1b0d0c543937a9ef74a
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-02-01 14:45:10 -0800

    building out the base schema without any protections for the fact that this is a prototype so password protection isn't a requiremnt to demonstrate the feature.

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    // messages: []
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User
