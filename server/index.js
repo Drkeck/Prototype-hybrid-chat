7500d56663f7623541698b6725e8637cfcca72bf
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-01-31 16:32:47 -0800

    added base server code, need to establish a database for making sample users and then build out the message section, this prototype is going to be a lot of work

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend Server running on ${PORT}! https://localhost:${PORT}`);
+})
