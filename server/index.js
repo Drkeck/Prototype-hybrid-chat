9f69e985dd5d86b84d84b93752286ebe2084ba24
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-01-31 17:33:30 -0800

    connected the database to the server script, from here we start building out schema's and also test things

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const db = require('./config/connection.js')
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,6 +13,8 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Backend Server running on ${PORT}! https://localhost:${PORT}`);
-})
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Backend Server running on ${PORT}! https://localhost:${PORT}`);
+    });
+});
