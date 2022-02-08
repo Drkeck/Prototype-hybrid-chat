f6903ed33fff9f65fd3cb7f49caa776071084e5c
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-02-07 16:38:32 -0800

    added the chat container and am starting to work on the chat feeditself

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ChatContainer from './Components/chatContainer';
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-3xl'>This Will Become A Chat App.</h1>
+      <ChatContainer/>
     </div>
   );
 }
---
+++ client/src/Components/chatContainer/index.js
@@ -0,0 +1,10 @@
+function ChatContainer() {
+
+    return(
+        <div className="w-3/5 mx-auto mt-8 bg-white shadow-xl">
+            <p>i am the chat container</p>
+        </div>
+    )
+}
+
+export default ChatContainer
