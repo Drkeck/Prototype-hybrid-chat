af01002c4f504104ce43ff7b4c7c944385280496
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-02-08 13:54:34 -0800

    small changes while i work on flow in my notebook, and also lunch!

--- client/src/Components/chatInput/index.js
+++ client/src/Components/chatInput/index.js
@@ -5,9 +5,15 @@ function ChatInput() {
     const [form, updateForm] = useState({message: ""});
 
     return(
-        <form className="w-full flex justify-around rounded-b-2xl border-t-2 border-dashed border-white h-14 text-lg" onChange={(e) => updateForm(e.target.value)} onSubmit={(e) => {
-            e.preventDefault()
-            console.log(form)
+        <form
+            // Setting up the added stuff like this really makes it more readable even if it makes the tag feel a little fat.
+            className="w-full flex justify-around rounded-b-2xl border-t-2 border-dashed border-white h-14 text-lg"
+            onChange={(e) => updateForm(e.target.value)}
+            onSubmit={(e) => {
+                e.preventDefault()
+                console.log(form)
+                // For now its just a log so we know the message is being captured properly
+                // There will be another logic bit here to have this message sent and rendered to the chat feed.
             }}>
             <input className="w-10/12 m-2 shadow-inner rounded-2xl resize-none px-2 border-none"/>
             <button className="rounded-full bg-purple w-2/12 m-2 text-white hover:text-black hover:bg-white">send</button>
