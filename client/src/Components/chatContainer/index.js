41530ee967f346e317a86f4db3ff484c877b9eb8
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-02-08 12:14:15 -0800

    added some basic functionality to the input side to capture basic inputs

--- client/src/Components/chatContainer/index.js
+++ client/src/Components/chatContainer/index.js
@@ -5,7 +5,7 @@ import ChatInput from "../chatInput"
 function ChatContainer() {
 
     return(
-        <div className="w-3/5 mx-auto mt-8 bg-white shadow-inner rounded-2xl bg-silver">
+        <div className="w-3/5 mx-auto mt-8 bg-[#dae0e5] shadow-inner rounded-2xl bg-silver">
             <ChatFeed />
             <ChatInput />
         </div>
--- client/src/Components/chatInput/index.js
+++ client/src/Components/chatInput/index.js
@@ -1,8 +1,15 @@
+import {useState} from 'react'
+
 function ChatInput() {
 
+    const [form, updateForm] = useState({message: ""});
+
     return(
-        <form className="w-full flex justify-around rounded-b-2xl border-t-2 border-dashed border-white h-14 text-lg">
-            <input className="w-10/12 m-2 shadow-inner rounded-2xl resize-none px-2"/>
+        <form className="w-full flex justify-around rounded-b-2xl border-t-2 border-dashed border-white h-14 text-lg" onChange={(e) => updateForm(e.target.value)} onSubmit={(e) => {
+            e.preventDefault()
+            console.log(form)
+            }}>
+            <input className="w-10/12 m-2 shadow-inner rounded-2xl resize-none px-2 border-none"/>
             <button className="rounded-full bg-purple w-2/12 m-2 text-white hover:text-black hover:bg-white">send</button>
         </form>
     )
