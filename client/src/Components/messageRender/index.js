baf12ef08b0dc378c50d9ea9d9269542c18748af
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-02-15 21:52:42 -0800

    added some dummy data to the component so i can link it up to the send button and try and get something printing

--- client/src/Components/messageRender/index.js
+++ client/src/Components/messageRender/index.js
@@ -1,5 +1,14 @@
 function MessageRender() {
     //logic for rendering each message goes here
+    return(
+        <div>
+            <image></image>
+            <div>
+                <h4>message</h4>
+            </div>
+            <p>sent</p>
+        </div>
+    )
 }
 
 export default MessageRender
