fc7f5bbb0fc0c5dd72402b20850f2800ce7b0018
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-02-17 10:26:26 -0800

    added the prop drilling needed to accomplish what i need for now

--- client/src/Components/chatFeed/index.js
+++ client/src/Components/chatFeed/index.js
@@ -1,4 +1,10 @@
-function ChatFeed() {
+function ChatFeed(props) {
+    const {
+        log,
+        updateLog
+    } = props;
+
+    console.log(log)
     //honestly unsure if i want to use the message render or just render it all here
     return(
         <section className="h-96">
