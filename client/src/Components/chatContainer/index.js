78d7b00f5086fef5eaf27fbe3040675704d81b09
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-02-21 16:20:26 -0800

    didn't want users connecting all the damn time so a useEffect was needed

--- client/src/Components/chatContainer/index.js
+++ client/src/Components/chatContainer/index.js
@@ -1,22 +1,24 @@
 import ChatFeed from "../chatFeed"
 import ChatInput from "../chatInput"
-import {useState} from "react"
+import { useEffect, useState } from "react"
 import { io } from "socket.io-client"
 
 
 function ChatContainer() {
     const socket = io();
-    const [form, updateForm] = useState({message: ""});
+    const [form, updateForm] = useState({ message: "" });
     const [chatLog, updateLog] = useState({})
-
-    socket.on("connect", () => {
-        console.log(socket.id)
+    useEffect(() => {
+        socket.on("connect", () => {
+            console.log(socket.id)
+        })
     })
 
-    return(
+
+    return (
         <div className="w-3/5 mx-auto mt-8 bg-[#dae0e5] shadow-inner rounded-2xl bg-silver">
-            <ChatFeed log={chatLog} updateLog={updateLog}/>
-            <ChatInput form={form} update={updateForm}/>
+            <ChatFeed log={chatLog} updateLog={updateLog} />
+            <ChatInput form={form} update={updateForm} />
         </div>
     )
 }
