536bf16c014bd653bf4ec1faabebcfe07fdab692
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-03-03 19:49:26 -0800

    starting to print messages properly and have a decent stlye while also need to set up timestamps too

--- client/src/Components/messageRender/index.js
+++ client/src/Components/messageRender/index.js
@@ -4,19 +4,31 @@ function MessageRender(props) {
         chatLog
     } = props;
 
-        // no one likes a blank chat.
+    // no one likes a blank chat.
     if (chatLog.length === 0) {
         return <h6>Start Messaging Now</h6>
+    }
+
+    function timestamp() {
+        const times = Date.now();
+        console.log(times)
     }
 
     return (
         <div>
             {chatLog?.map((message, index) => (
                 <div key={index}>
-                    <img></img>
-                    <p>{message.from}</p>
-                    <div>
-                        <h4>{message.message}</h4>
+                    <div className="text-left mx-5 flex flex-col my-2">
+                        <div className=" flex flex-row">
+                            <img src="https://via.placeholder.com/50" className="m-0" />
+                            <p className="mx-4">
+                                <b>{message.from}</b>
+                            </p>
+                            <p>{timestamp()}</p>
+                        </div>
+                        <div className="ml-16">
+                            <h4>{message.message}</h4>
+                        </div>
                     </div>
                 </div>
             ))}
