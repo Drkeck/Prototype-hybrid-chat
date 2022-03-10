007c32a4427ea635f396fa8551b5b7c677d6639f
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-03-09 18:58:57 -0800

    having a hard time wtih this at the moment will be working on it again tomorrow

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 // const path = require('path');
 const WebSocket = require('ws')
-const server = new WebSocket.Server({ port: '8080'})
+const server = new WebSocket.Server({ port: '8080' })
 
 const db = require('./config/connection.js')
 const PORT = process.env.PORT || 3001;
+
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -19,8 +21,8 @@ db.once('open', () => {
     });
 
     server.on('connection', wss => {
-    var id = 0
-    var lookup = {}
+        var id = 0
+        var lookup = {}
         server.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
                 // set clients id
@@ -30,10 +32,10 @@ db.once('open', () => {
                 // apply lookup to the wss
                 lookup[wss.id] = wss
 
-                client.send(JSON.stringify({message: "hello", from: "server"}))
+                client.send(JSON.stringify({ message: "hello", from: "server" }))
             }
         });
-        
+
         wss.on('message', message => {
             // we need to filter through online users and see if the socket is open for the target.
             // then we will need to either send the message to the them if its open, or skip that and just update the database.
