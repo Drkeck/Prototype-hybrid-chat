871e8b5a19a29ca490b6bce4ca8d628004ecc8c3
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-03-10 17:17:32 -0800

    need to make sure the server isn't taken down easily

--- server/index.js
+++ server/index.js
@@ -7,8 +7,6 @@ const server = new WebSocket.Server({ port: '8080' })
 const db = require('./config/connection.js')
 const PORT = process.env.PORT || 3001;
 
-
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -20,27 +18,22 @@ db.once('open', () => {
         console.log(`Backend Server running on ${PORT}! https://localhost:${PORT}`);
     });
 
-    server.on('connection', wss => {
-        var id = 0
-        var lookup = {}
+    server.on('connection', ws => {
         server.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
-                // set clients id
-                wss.id = id
-                // itterate on id
-                id++
-                // apply lookup to the wss
-                lookup[wss.id] = wss
-
                 client.send(JSON.stringify({ message: "hello", from: "server" }))
             }
         });
 
-        wss.on('message', message => {
+        ws.on('message', message => {
             // we need to filter through online users and see if the socket is open for the target.
             // then we will need to either send the message to the them if its open, or skip that and just update the database.
+            try {
             console.log(lookup[0].id);
             wss.send(message)
+            } catch(e) {
+                console.log(`something went wrong: ${e.message}`)
+            }
         });
     })
 });
